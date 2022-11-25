aa2a386920e16907a0192401dfc7a06ded2eb4bf
hanurakesh16 <hanurakesh11@gmail.com>
2022-11-25 16:17:37 +0530

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,11 +7,21 @@ import Exercises from '../components/Exercises'
 
 
 const Home = () => {
+  const [bodyPart, setBodyPart] = useState('all')
+    const [exercises, setExercises] = useState([]);
   return (
     <Box>
       <HeroBanner />
-      <SearchExercises /> 
-      <Exercises />
+      <SearchExercises 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+        setBodyPart={setBodyPart} 
+      /> 
+      <Exercises 
+        exercises={exercises} 
+        setExercises={setExercises} 
+        bodyPart={bodyPart} 
+      />
     </Box>
   )
 }
